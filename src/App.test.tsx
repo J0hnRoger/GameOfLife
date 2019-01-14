bee69252156f5c37dbca7484949d40ae92c53377
Jonathan Roger <jroger@boostmymail.com>
2019-01-14 22:48:15 +0100

    setup(Enzyme)

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import App from './App';
+
+const app = shallow(<App />);
+
+test('renders correctly', () => {
+
+});
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  render() {
+   return (
+    <div className='placehold'>
+      <h4 className="placehold__title">App</h4>
+    </div>
+   );
+ }
+}
+
+export default App;
